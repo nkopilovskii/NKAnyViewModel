1b34bb96d943682847ff3cfc8b91f14c0c933739
Nick Kopilovskii <nkopilovskii@gmail.com>
2019-07-04 13:25:55 +0300

    added AnyViewModel protocol

---
+++ NKAnyViewModel/Classes/NKAnyViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  AnyViewModel.swift
+//
+//  Created by Nick Kopilovskii on 7/4/19.
+//  Copyright © 2019 Nick Kopilovskii. All rights reserved.
+//
+
+import UIKit
+
+public protocol NKAnyViewModel {
+  static var viewAnyType: UIView.Type { get }
+  func setupAny(view: UIView)
+}
+
+public protocol NKViewModel: NKAnyViewModel {
+  associatedtype ViewType: UIView
+  func setup(view: ViewType)
+}
+
+public extension NKViewModel {
+  static var viewAnyType: UIView.Type {
+    return ViewType.self
+  }
+  
+  func setupAny(view: UIView) {
+    if let view = view as? ViewType {
+      setup(view: view)
+    } else {
+      //TODO: Handle this
+      assertionFailure("Error: \(#function) \(#file) \(#line)")
+    }
+  }
+}
+
